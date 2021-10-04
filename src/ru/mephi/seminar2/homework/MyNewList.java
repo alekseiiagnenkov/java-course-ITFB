747615c40ed00fba2b777d9cb1e9fa49d3a0f1df
Aleksei Iagnenkov <alekseiiagnenkov@gmail.com>
2021-10-04 23:20:28 +0300

    made a generic

--- src/ru/mephi/seminar2/homework/MyNewList.java
+++ src/ru/mephi/seminar2/homework/MyNewList.java
@@ -2,16 +2,14 @@ package ru.mephi.seminar2.homework;
 
 import ru.mephi.seminar1.classwork.MyList;
 
-import java.util.Comparator;
+public class MyNewList<T extends Comparable> extends MyList<T> {
 
-public class MyNewList extends MyList<Integer> {
-
-    MyNewList addAll(MyNewList other) {
-        MyNewList head = new MyNewList();
+    MyNewList<T> addAll(MyNewList<T> other) {
+        MyNewList<T> head = new MyNewList<>();
         while (this.size() > 0 || other.size() > 0) {
             if (other.size() > 0 && this.size() > 0) {
 
-                if ((Integer)other.get(0) < (Integer)this.get(0)) {
+                if (other.get(0).compareTo(this.get(0)) < 0) {
                     head.add(other.remove(0).getValue());
                 } else {
                     head.add(this.remove(0).getValue());
@@ -29,21 +27,21 @@ public class MyNewList extends MyList<Integer> {
 
     public static void main(String[] args) {
 
-        MyNewList list1 = new MyNewList();
+        MyNewList<Integer> list1 = new MyNewList<>();
         list1.add(-1);
         list1.add(1);
         list1.add(5);
         list1.add(6);
         list1.add(7);
 
-        MyNewList list2 = new MyNewList();
+        MyNewList<Integer> list2 = new MyNewList<>();
         list2.add(-3);
         list2.add(2);
         list2.add(5);
         list2.add(9);
         list2.add(10);
 
-        MyNewList head = list1.addAll(list2);
+        MyNewList<Integer> head = list1.addAll(list2);
         for (int i = 0; i < head.size(); i++)
             System.out.println(head.get(i));
     }
