42137d49d23f8ba173fb14d1e7aeffbe639ec843
Aleksei Iagnenkov <alekseiiagnenkov@gmail.com>
2021-10-15 19:21:32 +0300

    removed errors

--- src/ru/mephi/seminar3/classwork/Role.java
+++ src/ru/mephi/seminar3/classwork/Role.java
@@ -1,13 +1,11 @@
 package ru.mephi.seminar3.classwork;
 
-import lombok.val;
-
 public enum Role {
     STAFF(0.1),
     MANAGER(0.2),
     EXECUTIVE(0.3);
 
-    private double value;
+    private final double value;
 
     private Role(double value){
         this.value = value;
