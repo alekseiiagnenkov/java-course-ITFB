bd71b44842ca651f63e9268a622c2e8c3af5e567
Aleksei Iagnenkov <alekseiiagnenkov@gmail.com>
2021-10-23 02:05:32 +0300

    Fixed

--- src/ru/mephi/seminar2/homework/MyNewList.java
+++ src/ru/mephi/seminar2/homework/MyNewList.java
@@ -10,48 +10,20 @@ import ru.mephi.seminar1.classwork.MyList;
  */
 public class MyNewList<T extends Comparable> extends MyList<T> {
 
-    //TODO передеделать так, чтобы добавляла в this, а не в новый
-
     /**
      * Creates a new MyNewList that combines the elements of other and this.<br>
-     * Cleans other and this.
+     * Cleans other.
      *
      * @param other another MyNewList of the same type
-     * @return sorted MyNewList containing elements from this and other
      */
-    public MyNewList<T> addAll(MyNewList<T> other) {
-        MyNewList<T> head = new MyNewList<>();
-        while (this.size() > 0 || other.size() > 0) {
-            if (other.size() > 0 && this.size() > 0) {
-
-                if (this.get(0) == null) {
-                    this.remove(0);
-                }
-                if (other.get(0) == null) {
-                    other.remove(0);
-                }
-
-                if (other.get(0).compareTo(this.get(0)) < 0) {
-                    head.add(other.remove(0).getValue());
-                } else {
-                    head.add(this.remove(0).getValue());
-                }
-
-            } else if (this.size() != 0) {
-                head.add(this.remove(0).getValue());
-            } else if (other.size() != 0) {
-                head.add(other.remove(0).getValue());
-            }
-        }
-
-        return head;
-    }
-
-    public void addAll1(MyNewList<T> other) {
+    public void addAll(MyNewList<T> other) {
         while (other.size() != 0) {
             int i = 0;
-            while (this.get(i).compareTo(other.get(0)) > 0) {
-                i++;
+            for (; this.get(i).compareTo(other.get(0)) < 0; i++) {
+                if (i + 1 == this.size()) {
+                    i++;
+                    break;
+                }
             }
             this.add(other.remove(0).getValue(), i);
         }
@@ -65,7 +37,6 @@ public class MyNewList<T extends Comparable> extends MyList<T> {
         MyNewList<Integer> list1 = new MyNewList<>();
         list1.add(-1);
         list1.add(1);
-        list1.add(null);
         list1.add(6);
         list1.add(7);
 
@@ -76,11 +47,7 @@ public class MyNewList<T extends Comparable> extends MyList<T> {
         list2.add(9);
         list2.add(10);
 
-/*        MyNewList<Integer> head = list1.addAll(list2);
-        for (int i = 0; i < head.size(); i++)
-            System.out.println(head.get(i));*/
-
-        list1.addAll1(list2);
+        list1.addAll(list2);
         for (int i = 0; i < list1.size(); i++)
             System.out.println(list1.get(i));
     }
